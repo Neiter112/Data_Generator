002032238f98f854928eed708d6047330f441954
Roman Baryshnikov <neiter.nox@gmail.com>
2024-12-18 13:38:48 +0300

    upp

--- init.js
+++ init.js
@@ -2,5 +2,8 @@
 window.onload = function()
 {
     const initPerson = personGenerator.getPerson();
+    document.getElementById('surnameOutput').innerText = initPerson.surName;
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+    document.getElementById('middleNameOutput').innerText = initPerson.middleName;
 };
+
--- personGenerator.js
+++ personGenerator.js
@@ -20,6 +20,7 @@ const personGenerator = {
             "id_16": "Морозов"
         }
     }`,
+
     firstNameMaleJson: `{
         "count": 10,
         "list": {     
@@ -36,8 +37,29 @@ const personGenerator = {
         }
     }`,
 
+    firstNameFemaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Александра",
+            "id_2": "Екатерина",
+            "id_3": "Анастасия",
+            "id_4": "Мария",
+            "id_5": "Анна",
+            "id_6": "Юлия",
+            "id_7": "Елизавета",
+            "id_8": "Кристина",
+            "id_9": "Любовь",
+            "id_10": "Елена"
+        }
+    }`,
+
+
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
+
+    randomGender: function() {
+        return Math.floor(Math.random() * 2) == 0 ? this.GENDER_MALE : this.GENDER_FEMALE;
+     },
 
     randomIntNumber: (max = 1, min = 0) => Math.floor(Math.random() * (max - min + 1) + min),
 
@@ -47,24 +69,56 @@ const personGenerator = {
         return obj.list[prop];
     },
 
+    // Генерация имени 
     randomFirstName: function() {
-
+      if (this.person.gender == 'Мужчина') {
         return this.randomValue(this.firstNameMaleJson);
-
+      } 
+      else {
+        return this.randomValue(this.firstNameFemaleJson);
+       }
     },
 
-
-     randomSurname: function() {
-
-        return this.randomValue(this.surnameJson);
-
+    // Генерация фамилии 
+     randomSurName: function() {
+        if (this.person.gender == 'Мужчина') {
+          return this.randomValue(this.surnameJson);
+        }
+        else {
+          return this.randomValue(this.surnameJson) + 'а';
+        }  
     },
 
+    //Генерация Отчества
+    randomMiddleName: function() { 
+        middleName = this.randomValue(this.firstNameMaleJson);
+        if (this.person.gender == 'Мужчина') {
+            if (middleName.includes('й')) {
+                middleName = middleName.replace("й", "евич");
+            } else
+            if (middleName.includes('Никита')) {
+                middleName = middleName.replace("а", "ич");
+            } else
+                middleName = middleName + "ович";
+        } else
+ 
+        if (this.person.gender == 'Женщина') {
+            if (middleName.includes('й')) {
+                middleName = middleName.replace("й", "евна");
+            } else
+            if (middleName.includes('Никита')) {
+                middleName = middleName.replace("а", "ична");
+            } else
+                middleName = middleName + "овна"; 
+        }
+        return middleName;
+    },
 
     getPerson: function () {
         this.person = {};
         // this.person.gender = this.randomGender();
         this.person.firstName = this.randomFirstName();
+        this.person.surName = this.randomSurName();
         return this.person;
     }
 };
